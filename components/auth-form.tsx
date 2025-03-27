95b546e709ec2b1f78036542e0e07be98492428d
Prakash Shukla <prakash@infocusp.com>
2025-03-27 19:29:59 +0530

    login and register flow

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -10,49 +10,90 @@ export default function AuthForm() {
 
   const register = async () => {
     try {
-      const response = await fetch("/api/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username }),
+      const data = await navigator.credentials.create({
+        publicKey: {
+          challenge: new Uint8Array(32),
+          rp: {
+            name: "my backend server",
+            id: "localhost",
+          },
+          user: {
+            id: new Uint8Array(32),
+            name: username,
+            displayName: "",
+          },
+          pubKeyCredParams: [
+            { type: "public-key", alg: -7 },
+            { type: "public-key", alg: -257 },
+          ],
+        },
       });
-      const data = await response.json();
-      setMessage(data.message || "Registered successfully");
+
+      if (data?.rawId) {
+        localStorage.setItem(
+          "userRawId",
+          JSON.stringify([...new Uint8Array(data.rawId)])
+        );
+        setMessage("Registered successfully");
+      } else {
+        setMessage("Registration failed");
+      }
     } catch (error) {
-      setMessage("Registration failed");
+      console.log(error);
+      setMessage("Registration error");
     }
   };
 
   const login = async () => {
     try {
-      const response = await fetch("/api/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username }),
+      const storedRawId = localStorage.getItem("userRawId");
+      if (!storedRawId) {
+        setMessage("Please Register First");
+        return;
+      }
+
+      const data = await navigator.credentials.get({
+        publicKey: {
+          challenge: new Uint8Array(32),
+          rpId: "localhost",
+          allowCredentials: [
+            {
+              id: new Uint8Array(JSON.parse(storedRawId)).buffer,
+              type: "public-key",
+            },
+          ],
+        },
       });
-      const data = await response.json();
-      if (response.ok) {
+
+      if (data) {
         localStorage.setItem("username", username);
         router.push("/profile");
       } else {
-        setMessage(data.error || "Login failed");
+        setMessage("Login failed");
       }
     } catch (error) {
-      setMessage("Login failed");
+      console.log(error);
+      setMessage("Login error");
     }
   };
+
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg w-96">
       <h2 className="text-2xl font-semibold mb-4">WebAuthn Auth</h2>
       <input
         type="text"
         placeholder="Username"
-        className="w-full p-2 border rounded mb-4"
+        className="w-full p-2 border rounded"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
+      <span className="text-xs italic text-gray-600 leading-none">
+        Username is optional for this demo as we are not storing anything on the
+        backend.
+      </span>
       <button
         onClick={register}
-        className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 mb-2"
+        className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 mb-2 mt-4"
       >
         Register New Pass Key
       </button>
@@ -62,7 +103,11 @@ export default function AuthForm() {
       >
         Login
       </button>
-      {message && <p className="mt-4 text-center text-gray-700">{message}</p>}
+      {message && (
+        <p className="mt-4 text-lg text-center text-red-700 font-bold">
+          {message}
+        </p>
+      )}
     </div>
   );
 }
